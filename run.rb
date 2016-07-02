06b159945c29e3283e18936461be161a4a7f77b7
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-02 01:12:28 +0100

    added start and finish

--- run.rb
+++ run.rb
@@ -49,21 +49,21 @@ while @dave.has_won? == false &&
 
 
   system("clear")
-  print @track.track.compact.join( " " )
+  print "1" + @track.track.compact.join( " " ) + "*"
   puts
-  print @track2.track.compact.join( " " )
+  print "2" + @track2.track.compact.join( " " ) + "F"
   puts
-  print @track3.track.compact.join( " " )
+  print "3" + @track3.track.compact.join( " " ) + "I"
   puts
-  print @track4.track.compact.join( " " )
+  print "4" + @track4.track.compact.join( " " ) + "N"
   puts
-  print @track5.track.compact.join( " " )
+  print "5" + @track5.track.compact.join( " " ) + "I"
   puts
-  print @track6.track.compact.join( " " )
+  print "6" + @track6.track.compact.join( " " ) + "S"
   puts
-  print @track7.track.compact.join( " " )
+  print "7" + @track7.track.compact.join( " " ) + "H"
   puts
-  print @track8.track.compact.join( " " )
+  print "8" + @track8.track.compact.join( " " ) + "*"
   puts
   @race.go
   @race2.go
